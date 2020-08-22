5f823769099cc5866c9ddaa8f392c9b57f64249b
Cesar Gomez <zootechdrum@gmail.com>
2020-08-22 12:10:23 -0700

    removed files

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,25 @@
-console.log("Hello");
+import { GraphQLServer } from "graphql-yoga";
+
+const typeDefs = `
+type Query {
+    hello: String!
+    name: String!
+}`;
+
+//Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return "This is my first query";
+    },
+    name() {
+      return "Cesar Gomez";
+    },
+  },
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start(() => {
+  console.log("Server is up and running");
+});
