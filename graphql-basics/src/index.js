0712e667516e49173756cb413193f76c986eb8dd
Cesar Gomez <zootechdrum@gmail.com>
2020-08-22 20:58:51 -0700

    added resolver and added type def for comments

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -5,9 +5,34 @@ const users = [
   { id: "2", name: "lisa Gomez", email: "zootechdrum@gmail.com", age: 27 },
 ];
 
+const posts = [
+  {
+    id: "1",
+    title: "cesar Gomrz",
+    body: "lorem ipsum hdfksjdhsk  dfksjhdfjsk",
+    published: true,
+    author: "1",
+  },
+  {
+    id: "2",
+    title: "farts",
+    body: "lorem ipsum hdfksjdhsk  dfksjhdfjsk",
+    published: false,
+    author: "2",
+  },
+];
+
+const comments = [
+  { id: "44", text: "hole" },
+  { id: "4", text: "shits" },
+  { id: "6", text: "shits" },
+];
+
 const typeDefs = `
 type Query {
   users (query: String): [User]
+  posts (query: String): [Post]
+  comments: [!Comment]!
     me: User! 
     post: Post!
 }
@@ -15,25 +40,45 @@ type Post {
   id: ID!
   title: String!
   body: String!
-  published: String!
+  published: Boolean
+  author: User!
 }
 type User {
   id: ID!
   name: String!
   email: String!
   age: Int
+  posts: [Post!]!
+}
+
+type Comment {
+  id: ID!
+  text: String!
 }
 `;
 
 //Resolvers
 const resolvers = {
   Query: {
+    comments(parent, args, ctx, info) {
+      if (!args.query) {
+        return comments;
+      }
+    },
     users(parent, args, ctx, info) {
       if (!args.query) {
         return users;
       }
       return users.filter((user) => {
         return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts;
+      }
+      return posts.filter((post) => {
+        return post.title.toLowerCase().includes(args.title.toLowerCase());
       });
     },
     me() {
@@ -51,6 +96,20 @@ const resolvers = {
         body: "Query",
         published: "7/7/70",
       };
+    },
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author;
+      });
+    },
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter((post) => {
+        return post.author === parent.id;
+      });
     },
   },
 };
--- resolvers.js
+++
@@ -1,7 +0,0 @@
-const db = require("./db");
-
-const Query = {
-  test: () => "Test Success, GraphQL server is up & running !!",
-  students: () => db.students.list(),
-};
-module.exports = { Query };
