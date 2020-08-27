3973489babd530d4bcc3c9111ea285f203e1e16d
Cesar Gomez <zootechdrum@gmail.com>
2020-08-27 15:40:21 -0700

    completed my first subscription

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,16 +1,20 @@
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, pubSub, PubSub } from "graphql-yoga";
 import Query from "./resolvers/Query";
 import Mutation from "./resolvers/Mutations";
+import Subscription from "./resolvers/Subscription";
 import User from "./resolvers/User";
 import Post from "./resolvers/Post";
 import Comment from "./resolvers/Comment";
 import db from "./db";
+
+const pubsub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers: {
     Query,
     Mutation,
+    Subscription,
     User,
     Post,
     Comment,
@@ -18,6 +22,7 @@ const server = new GraphQLServer({
 
   context: {
     db,
+    pubsub,
   },
 });
 
