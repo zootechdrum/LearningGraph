7ee57615ee1adcecc3088f3ce2df9f73d3cccb4c
Cesar Gomez <zootechdrum@gmail.com>
2020-08-28 22:27:22 -0700

    added subscription for every condition that might happen on a post

--- graphql-basics/src/resolvers/Mutations.js
+++ graphql-basics/src/resolvers/Mutations.js
@@ -76,11 +76,17 @@ const Mutation = {
       ...args.data,
     };
     db.posts.push(post);
-    if (args.data.published) pubsub.publish(`post`, { post });
+    if (args.data.published)
+      pubsub.publish(`post`, {
+        post: {
+          mutation: "CREATED",
+          data: post,
+        },
+      });
     return post;
   },
 
-  deletePost(parent, args, { db }, info) {
+  deletePost(parent, args, { db, pubsub }, info) {
     const postIndex = db.posts.findIndex((post) => {
       return post.id === args.id;
     });
@@ -88,16 +94,25 @@ const Mutation = {
     if (postIndex === -1) {
       throw new Error("Post not found");
     }
-    const deletedPost = db.posts.splice(postIndex, 1);
+    const [deletedPost] = db.posts.splice(postIndex, 1);
 
     db.comments = db.comments.filter((comment) => {
       comment.postId !== args.id;
     });
-    return deletedPost[0];
+    if (deletedPost.published) {
+      pubsub.publish("post", {
+        post: {
+          mutation: "DELETED",
+          data: deletedPost,
+        },
+      });
+    }
+    return deletedPost;
   },
-  updatePost(parent, args, { db }, info) {
+  updatePost(parent, args, { db, pubsub }, info) {
     const { id, data } = args;
     const post = db.posts.find((post) => post.id === id);
+    const originalPost = { ...post };
 
     if (!post) {
       throw Error("Post does not exist");
@@ -113,7 +128,32 @@ const Mutation = {
 
     if (typeof data.published === "boolean") {
       post.published = data.published;
+
+      if (originalPost.published && !post.published) {
+        //Deleted
+        pubsub.publish("post", {
+          post: {
+            mutation: "DELETED",
+            data: originalPost,
+          },
+        });
+      } else if (!originalPost.published && post.published) {
+        pubsub.publish("post", {
+          post: {
+            mutation: "CREATED",
+            data: post,
+          },
+        });
+      }
+    } else if (post.published) {
+      pubsub.publish("post", {
+        post: {
+          mutation: "UPDATED",
+          data: post,
+        },
+      });
     }
+
     return post;
   },
 
