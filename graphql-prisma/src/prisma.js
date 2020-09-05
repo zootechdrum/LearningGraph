68508c3ff446680c6dfb8a2929a59ed1c78015ca
Cesar Gomez <zootechdrum@gmail.com>
2020-09-04 21:08:02 -0700

    used binding for validating data before it goes and searched for it

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -5,17 +5,36 @@ const prisma = new Prisma({
   endpoint: "http://localhost:4466",
 });
 
-// prisma.query.users(null, `{id name}`).then((data) => {
-//   console.log(data);
-// });
+const createPostForUser = async (authorId, data) => {
+  const userExist = await prisma.exists.User({ id: authorId });
 
-// prisma.query.comments(null, `{id text author {id name}}`).then((data) => {
-//   console.log(JSON.stringify(data, undefined, 2));
-// });
+  if (!userExist) {
+    throw new Error("User not found");
+  }
 
-//prisma.query.posts(null, `{id title}`).then((data) => console.log(data));
+  const post = await prisma.mutation.createPost(
+    {
+      data: {
+        ...data,
+        author: {
+          connect: {
+            id: authorId,
+          },
+        },
+      },
+    },
+    `{author {id name email post {id title published}}}`
+  );
+
+  return post;
+};
 
 const updatePost = async (postId, data) => {
+  const postExist = await prisma.exists.Post({ id: postId });
+
+  if (!postExist) {
+    throw new Error("Post not found");
+  }
   const post = await prisma.mutation.updatePost(
     {
       data: {
@@ -25,26 +44,32 @@ const updatePost = async (postId, data) => {
         id: postId,
       },
     },
-    `{id title body published author { id }}`
-  );
-  let author = post.author.id;
-
-  const user = await prisma.query.user(
-    {
-      where: { id: author },
-    },
-    `{id name email post { id title published} }`
+    `{ author { id name email post {id title published} }}`
   );
 
-  return user;
+  return post;
 };
+
+// createPostForUser("ckekfq7g7047z0892c24o1v7m", {
+//   title: "Great books to read",
+//   body: "Good book",
+//   published: true,
+// })
+//   .then((user) => {
+//     console.log(JSON.stringify(user, undefined, 2));
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 updatePost(`ckekgp55e04b60892r06jgs3l`, {
   title: "I dont have time",
   published: true,
-}).then((user) => {
-  console.log(JSON.stringify(user, undefined, 2));
-});
+})
+  .then((user) => {
+    console.log(JSON.stringify(user, undefined, 2));
+  })
+  .catch((err) => console.log(err.mesage));
 
 // prisma.mutation
 //   .createPost(
